aa3055263d34b82e08298c3472ce8fd8064fd5d1
Murodbekovich <amatovbunyodbek671@gmail.com>
2026-04-14 01:51:08 +0500

    fix: swagger schemas and parameters

--- src/infrastructure/swagger/swaggerConfig.js
+++ src/infrastructure/swagger/swaggerConfig.js
@@ -5,7 +5,7 @@ const options = {
     definition: {
         openapi: '3.0.0',
         info: {
-            title: 'E-DrawGuide - LMS Professional API',
+            title: 'E-DrawGuide - LMS API Panel',
             version: '1.0.0'
         },
         servers: [{ url: '/api/v1' }],
