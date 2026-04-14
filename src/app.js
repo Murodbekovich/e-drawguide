38751f690480aaf51ffea887424014c62e67bc60
Murodbekovich <amatovbunyodbek671@gmail.com>
2026-04-14 07:46:19 +0500

    frontent

--- src/app.js
+++ src/app.js
@@ -8,12 +8,12 @@ const hpp = require('hpp');
 const xss = require('xss-clean');
 const Sentry = require('@sentry/node');
 const swaggerUi = require('swagger-ui-express');
-const swaggerSpecs = require('./infrastructure/swagger/swaggerConfig');
+const swaggerJSDoc = require('swagger-jsdoc');
 
 const routes = require('./app/routes/api/v1/index');
 const errorHandler = require('./app/middlewares/errorHandler');
 const logger = require('./utils/logger');
-const { apiLimiter, authLimiter } = require('./app/middlewares/rateLimiters');
+const { apiLimiter } = require('./app/middlewares/rateLimiters');
 const setLang = require('./app/middlewares/setLang');
 
 const app = express();
@@ -24,6 +24,11 @@ if (process.env.SENTRY_DSN) {
     Sentry.init({ dsn: process.env.SENTRY_DSN });
     app.use(Sentry.Handlers.requestHandler());
 }
+
+app.use('/api-docs', (req, res, next) => {
+    res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdnjs.cloudflare.com; style-src 'self' 'https:' 'unsafe-inline'; img-src 'self' data: https:;");
+    next();
+});
 
 app.use(helmet({
     contentSecurityPolicy: false,
@@ -37,10 +42,37 @@ app.use(morgan('combined', { stream: logger.stream }));
 app.use(cors({ origin: '*', credentials: true }));
 
 app.use('/api/', apiLimiter);
-app.use('/api/v1/auth/login', authLimiter);
-app.use('/api/v1/auth/register', authLimiter);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs, {
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+        title: 'E-DrawGuide Professional API',
+        version: '1.0.0'
+    },
+    servers: [{ url: '/api/v1' }],
+    tags: [
+        { name: 'ADMIN' },
+        { name: 'MOBILE' }
+    ],
+    components: {
+        securitySchemes: {
+            bearerAuth: {
+                type: 'http',
+                scheme: 'bearer',
+                bearerFormat: 'JWT'
+            }
+        }
+    },
+    security: [{ bearerAuth: [] }]
+};
+
+const swaggerOptions = {
+    swaggerDefinition,
+    apis: [path.join(__dirname, './app/routes/api/v1/*.js'), path.join(__dirname, './docs/*.yaml')],
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, {
     swaggerOptions: {
         persistAuthorization: true,
         docExpansion: 'list',
